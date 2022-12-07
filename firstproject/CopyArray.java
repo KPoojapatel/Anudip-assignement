8f8f615ea8bc374c2c668872a7faafc1b57050ab
KPoojapatel <120014006+KPoojapatel@users.noreply.github.com>
2022-12-07 16:26:05 +0530

    Add files via upload

---
+++ firstproject/CopyArray.java
@@ -0,0 +1,22 @@
+package firstproject;
+
+public class CopyArray {  
+    public static void main(String[] args) {      
+            
+        int [] arr1 = new int [] {1, 2, 3, 4, 5};     
+        int arr2[] = new int[arr1.length];    
+        for (int i = 0; i < arr1.length; i++) {   
+            arr2[i] = arr1[i];   
+        }      
+        System.out.println("first array: ");  
+        for (int i = 0; i < arr1.length; i++) {   
+           System.out.print(arr1[i] + " ");  
+        }   
+          
+        System.out.println();    
+        System.out.println(" copied array: ");  
+        for (int i = 0; i < arr2.length; i++) {   
+           System.out.print(arr2[i] + " ");  
+        }   
+    }  
+}  
