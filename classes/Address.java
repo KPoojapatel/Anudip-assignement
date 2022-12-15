d41a8d39bc3acb91a7e84e7ea33e654d3312a104
KPoojapatel <120014006+KPoojapatel@users.noreply.github.com>
2022-12-15 18:28:36 +0530

    Add files via upload

---
+++ classes/Address.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Address {
+	int Hno;
+	String city;
+	String state;
+	Address(int Hno,String city, String state) {
+		this.Hno=Hno;
+		this.city=city;
+		this.state=state;
+	}
+	@Override
+	public String toString() {
+		return "Address [Hno=" + Hno + ", city=" + city + ", state=" + state + "]";
+	}
+	
+	
+
+}
---
+++ classes/Employee.java
@@ -0,0 +1,17 @@
+package classes;
+//Employee class which inherit member class
+public class Employee extends Member{
+private String specialization;
+
+//Parameterized constructor 
+
+public Employee (String name,int age,String phoneNumber,Address address,int salary,String specialization)
+{
+	super(name,age,phoneNumber,address,salary);
+	
+	this.specialization=specialization;
+	System.out.println("specialization:" +this.specialization);
+
+}
+
+}
