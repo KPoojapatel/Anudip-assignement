0f5073317e05967dbb758ec474b428ea0fe584d3
KPoojapatel <120014006+KPoojapatel@users.noreply.github.com>
2022-12-22 17:19:37 +0530

    Add files via upload

---
+++ oops/Person.java
@@ -0,0 +1,19 @@
+package oops;
+public class Person {
+	//Declaration 
+	String Name;
+	int Age;
+	String Mob;
+	//Default Constructor
+	public Person(){
+		System.out.println("Default Constructor.....");
+		
+	}
+	//Parameterized Constructor
+	public Person(String Name,int Age,String Mob) {
+		this.Name=Name;
+		this.Age=Age;
+		this.Mob=Mob;
+	}
+
+}
